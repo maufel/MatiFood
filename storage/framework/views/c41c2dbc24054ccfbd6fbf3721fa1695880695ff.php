7bd084e9f6f8f7ba6e3e1218a65591ff2702441d
Maurizio <maufel@gmail.com>
2020-01-09 22:47:06 +0100

    Add files via upload

---
+++ storage/framework/views/c41c2dbc24054ccfbd6fbf3721fa1695880695ff.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="<?php echo e(app()->getLocale()); ?>">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Laravel React application</title>
+        <link href="<?php echo e(mix('css/app.css')); ?>" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+    <h2 style="text-align: center"> Laravel and React application </h2>
+        <div id="root"></div>
+        <script src="<?php echo e(mix('js/app.js')); ?>" ></script>
+    </body>
+</html><?php /**PATH /var/www/html/matifood02/resources/views/welcome.blade.php ENDPATH**/ ?>
---
+++ webpack.mix.js
@@ -0,0 +1,19 @@
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.react('resources/js/app.js', 'public/js')
+   .sass('resources/sass/app.scss', 'public/css');
+
+mix.browserSync('localhost:8000');
+//Add your own php artisan serve's url and port
+//For me url is localhost or 127.0.0.1 and port is 8000
